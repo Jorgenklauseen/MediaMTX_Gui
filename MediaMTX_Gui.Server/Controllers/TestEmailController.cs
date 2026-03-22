223107a6e3dcceb2a393d59d28178c57d4322ed6
Jorgen Klausen <jorgennk@student.uia.no>
2026-03-22 14:09:31 +0100

    testing email with UiA's email server

---
+++ MediaMTX_Gui.Server/Controllers/TestEmailController.cs
@@ -0,0 +1,26 @@
+namespace MediaMTX_Gui.Server.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+[ApiController]
+public class TestEmailController : ControllerBase
+{
+    [HttpPost("api/test-email")]
+    public async Task<IActionResult> Send()
+    {
+        var message = new MimeMessage();
+        message.From.Add(MailboxAddress.Parse("no-reply@uia.no"));
+        message.To.Add(MailboxAddress.Parse("jorgennk@uia.no"));
+        message.Subject = "Test";
+        message.Body = new TextPart("plain") { Text = "Testmelding" };
+
+        using var client = new SmtpClient();
+        await client.ConnectAsync("smtp.uia.no", 587, MailKit.Security.SecureSocketOptions.StartTls);
+        await client.SendAsync(message);
+        await client.DisconnectAsync(true);
+
+        return Ok("Sendt!");
+    }
+}
--- mediamtx_gui.client/src/pages/Recordings.tsx
+++ mediamtx_gui.client/src/pages/Recordings.tsx
@@ -3,6 +3,17 @@ function Recordings() {
     <div>
       <h1>Recordings Page</h1>
       <p>This is the recordings page</p>
+
+      <div>
+        <button
+          onClick={async () => {
+            const res = await fetch("/api/test-email", { method: "POST" });
+            alert(res.ok ? "Sendt!" : "Feil");
+          }}
+        >
+          Test epost
+        </button>
+      </div>
     </div>
   );
 }
